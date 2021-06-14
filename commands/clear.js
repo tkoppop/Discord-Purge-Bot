52d74a43b13ff6078ee708f2944b8359af3557e9
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-14 14:18:23 -0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -8,7 +8,6 @@ module.exports = {
         if(isNaN(args[0]))return message.reply("please enter a real number!");
         if(args[0] > 1000) return message.reply("You cannot delete more than 100 messages!");
         if( args[0] < 1) return message.reply("You must delete at least one message!");
-
         await message.channel.messages.fetch({limit: args[0]}).then(messages => {
             message.channel.bulkDelete(messages);
         })
