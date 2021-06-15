77dfc8a414311f74f63815b294f26b9431c61dd1
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-15 13:22:20 -0700

    hidden discord bot token

--- main.js
+++ main.js
@@ -39,8 +39,8 @@ client.on('message', message => {
             console.error(error);
             message.reply('there was an error trying to execute that command!');
         }
-	}
+	} 
 });
 
-client.login();
+client.login(token);
 
